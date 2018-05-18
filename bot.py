b4a77d5ca11c3a18e170153bccae9d9ad4ecd69c
ItzSchwifty <38725236+ItzSchwifty@users.noreply.github.com>
2018-05-18 18:01:09 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -42,8 +42,12 @@ class Main_Commands():
 
 @client.event 
 async def on_message(message):
+        if message.content.upper().startswith("/SUMMON"):
+                    await client.send_message(message.channel, "Dont put two bots in the same VC please. Beep Boop")
         if message.content.upper().startswith("/SUPPORT"):
-                    await client.send_message(message.channel, "Heres the server :) https://discord.gg/2ga7k6r")                    
+                    await client.send_message(message.channel, "Heres the server :) https://discord.gg/2ga7k6r")
+        if message.content.upper().startswith("/PLAY"):
+                    await client.send_message(message.channel, "**Awaiting Data...**")  
         if message.content.upper().startswith("/PING"):
                     userID = message.author.id       
                     await client.send_message(message.channel, "<@%s> Pong! :ping_pong:" % (userID))
@@ -96,6 +100,10 @@ async def on_message(message):
                                                                              "My sources say no :8ball:",
                                                                              "Outlook not so good :8ball:",
                                                                              "Very doubtful :8ball:"]))
+        if message.content.upper().startswith("/TF2"):               
+                    embed = discord.Embed(title="Heres the website", description="http://www.teamfortress.com/", color=7506394)
+                    embed.set_footer(text='Support The Bot | SchwiftyBot ')
+                    await client.send_message(message.channel, embed=embed)
         if message.content.upper().startswith("/REPOSIT"):
                     await client.send_message(message.channel, "This is still in progress but its just a good look inside my bot")
                     await client.send_message(message.channel, "https://github.com/ItzSchwifty/ScwhiftyBot")
@@ -104,13 +112,12 @@ async def on_message(message):
                     await client.send_message(message.channel, random.choice(["<@%s> Its a heads!"  % (userID),
                                                                              "<@%s> Its a tails!" % (userID)]))
         if message.content.upper().startswith("//BOT"):
-                    embed=discord.Embed(title="Hello! I'm Schwifty the Bot", url="http://schwiftybot.website2.me/", description = "I was created by a person named Schwifty#7692. This bot is under heavy development by only a single creator. The creator is also self taught and plans to spend absolutely no money at all. Anyways I created this bot mainly for fun at first but I instead put my time and effort into this. To support the bot and the creator, please go to our dashboard and send the invites to other servers, this will make e very happy and maybe feel a little bit succesful. Anyways have a good day everyone", color=0x1b9aa0)
+                    embed=discord.Embed(title="Hello! I'm Schwifty the Bot", url="http://schwiftybot.website2.me/", description = "I was created by a person named Schwifty#7692. This bot is under heavy development by only a single creator. The creator is also self taught and plans to spend absolutely no money at all. Anyways I created this bot mainly for fun at first but I instead put my time and effort into this. To support the bot and the creator, please go to our dashboard and send the invites to other servers, this will make me very happy and maybe feel a little bit succesful. Anyways have a good day everyone", color=0x1b9aa0)
                     embed.set_footer(text='Support The Bot | SchwiftyBot')
                     await client.send_message(message.channel, embed=embed)
         if message.content.upper().startswith("/INFO"):
                     embed=discord.Embed(title="Info Commands", description="This shows you other extended information and usages of the info commands", color=0x1b9aa0)
                     embed.set_author(name="SchwiftyBot", url="http://schwiftybot.website2.me", icon_url="https://cdn.discordapp.com/attachments/323726246116589570/443672159064162314/Cabbage.jpg")
-                    embed.set_thumbnail(url="https://qph.fs.quoracdn.net/main-qimg-cd1da8f17d8d317e5a084282a2758405")
                     embed.add_field(name="//bot", value="Shows the bot's information", inline=False)
                     embed.add_field(name="//<command>", value="Shows the purpose of the command and its usage. The two types are Ordinary which means no specific usage and the second one is Specific which means you have to use it in a specific way. Example //version", inline=False)                    
                     embed.set_footer(text='Support The Bot | SchwiftyBot ')
@@ -223,6 +230,20 @@ async def on_message(message):
                     embed.add_field(name=":arrow_forward: Cleans the entire queue (Be in a voice chat first and say /summon)", value="No usage (Ordinary)", inline=False)
                     embed.set_footer(text='Support The Bot | SchwiftyBot ')
                     await client.send_message(message.channel, embed=embed)
+        if message.content.upper().startswith("//RLINKS"):
+                    embed=discord.Embed(title="Info Commands", description="This shows you other extended information and usages of the info commands", color=0x1b9aa0)
+                    embed.set_author(name="SchwiftyBot", url="http://schwiftybot.website2.me", icon_url="https://cdn.discordapp.com/attachments/323726246116589570/443672159064162314/Cabbage.jpg")
+                    embed.add_field(name=":arrow_forward: Directly gives you the sandbox links to the subreddit of diep.io", value="No usage (Ordinary)", inline=False)
+                    embed.set_footer(text='Support The Bot | SchwiftyBot ')
+                    await client.send_message(message.channel, embed=embed)
+        if message.content.upper().startswith("//TF2"):
+                    embed=discord.Embed(title="Info Commands", description="This shows you other extended information and usages of the info commands", color=0x1b9aa0)
+                    embed.set_author(name="SchwiftyBot", url="http://schwiftybot.website2.me", icon_url="https://cdn.discordapp.com/attachments/323726246116589570/443672159064162314/Cabbage.jpg")
+                    embed.add_field(name=":arrow_forward: Gives you the announcements, updates and the website itself", value="No usage (Ordinary)", inline=False)
+                    embed.set_footer(text='Support The Bot | SchwiftyBot ')
+                    await client.send_message(message.channel, embed=embed)
+                        
+                        
                         
                         
                         
@@ -247,7 +268,7 @@ async def on_message(message):
                     embed.add_field(name=":arrow_forward: Entertainment :video_game:", value="/8ball (Ohhh whats this?)                                                                                                                          /coinflip (Flips a coin)                                                                                                                                                /ping (Pong!)                                                                                                                                                                                                                                                                                   /shoot <@user>(Shoot someone)" , inline=False)
                     embed.add_field(name=":arrow_forward: Moderation :hotsprings:", value="Coming soon", inline=False)
                     embed.add_field(name=":arrow_forward: Music :headphones:", value="/id <@user> (Gives your own ID)                                                                                       /play <song> (Plays the song)(Be in a voice chat first)                                                                       /queue (Gives the song list(s))                                                                                           /search (Enable you to search some stuff)                                                                                     /skip (Skips the song playing right now)                                                                                          /summon (Summons the bot) (CONNECT TO A VOICE CHANNEL)                                                                                               /clean (Cleans the entire queue)                                                                                                                                                   /perms (Shows you the permissions)", inline=False)
-                    embed.add_field(name=":arrow_forward: Games :game_die:", value="/rlinks (Shows you the newest diep.io sanbox links)", inline=False)
+                    embed.add_field(name=":arrow_forward: Games :game_die:", value="/rlinks (Shows you the newest diep.io sanbox links)                                                                                                /tf2 (Gives you the website)", inline=False)
                     embed.set_footer(text='Support The Bot | SchwiftyBot ')
                     await client.send_message(message.channel, embed=embed)
                     os.system('cls' if os.name=='nt' else 'clear')
@@ -275,7 +296,6 @@ async def on_message(message):
 
  
 
-#Not This Time Bud      
-client.run("Token")
-
+          
+client.run("token")
 
